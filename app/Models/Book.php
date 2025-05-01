1ba5ef59d5f1fd55374d76f4a4c8da7492997097
Raka Hikmah Ramadhan <rakahikmah46@gmail.com>
2025-05-01 23:06:45 +0700

    Add Book Controller Crud

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Review;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Book extends Model
 {
@@ -16,5 +19,81 @@ class Book extends Model
         return $this->hasMany(Review::class);
     }
 
-    
+
+    public function scopeTitle(Builder $query, string $title)
+    {
+        return $query->where('title', 'like', "%$title%");
+    }
+
+
+    public function scopePopular(Builder $query, $from = null, $to = null)
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null)
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $min)
+    {
+        return $query->withCount('reviews')->having('reviews_count', '>=', $min);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            return $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            return $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            return $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
+    public function scopeWithRecentReviews(Builder $query, \Closure $interval): Builder
+    {
+        return $query->whereHas(
+            'reviews',
+            function (Builder $q) use ($interval) {
+                $q->whereBetween(
+                    'created_at',
+                    [$interval(now()), now()]
+                );
+            }
+        );
+    }
+
+    public function scopePopularLastMonth(Builder $query)
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopePopularLast6Months(Builder $query)
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query)
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query)
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,27 @@
 // hallo 
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::resource('books', BookController::class);
+
+
+Route::get('test-redis', function () {
+    // Cek apakah cache Redis tersedia dan simpan nilai
+    Cache::store('redis')->put('test_key', 'Hello, Redis!', 10);
+
+    // Ambil nilai dari cache
+    $value = Cache::store('redis')->get('test_key');
+
+    // Kembalikan response untuk melihat apakah cache berhasil tersimpan dan diambil
+    return response()->json([
+        'message' => 'Cache test completed!',
+        'cached_value' => $value,
+    ]);
 });
